ed4087cb7aa29803513c6564434768f72c77e149
benjaminzhuo <benjaminzhuo@172.24.89.36>
2017-04-12 15:22:04 -0400

    Changed some XML properties and created backbone of all but TApage

--- src/csg/workspace/CourseSiteGeneratorWorkspace.java
+++ src/csg/workspace/CourseSiteGeneratorWorkspace.java
@@ -46,8 +46,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.FlowPane;
 import properties_manager.PropertiesManager;
-
-
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ColorPicker;
 /**
  *
  * @author benjaminzhuo
@@ -137,11 +137,89 @@ public class CourseSiteGeneratorWorkspace extends AppWorkspaceComponent{
     TextField locationTextField;
     ComboBox firstTAComboBox;
     ComboBox secondTAComboBox;
-    Button addUpdate;
-    Button clear;
+    Button recitationAddUpdateButton;
+    Button recitationClearButton;
     ////////////////////////////////Schedule///////////////////////////////////
     
-           
+    VBox schedulePagePane;
+    Label scheduleLabel;
+    Label calendarBoundariesLabel;
+    Label startingMondayLabel;
+    Label endingFridayLabel;
+    Label scheduleItemsLabel;
+    TableColumn typeColumn;
+    TableColumn dateColumn;
+    TableColumn titleColumn;
+    TableColumn topicColumn;
+    TableView scheduleItemsTable;
+    Label scheduleAddEditLabel;
+    Label typeLabel;
+    Label dateLabel;
+    Label timeLabel;
+    Label scheduleTitleLabel;
+    Label topicLabel;
+    Label linkLabel;
+    Label criteriaLabel;
+    Button scheduleAddUpdateButton;
+    Button scheduleClearButton;
+    ComboBox typeComboBox;
+    DatePicker scheduleDate;
+    DatePicker mondayDate;
+    DatePicker fridayDate;
+    TextField scheduleTimeTextField;
+    TextField scheduleTitleTextField;
+    TextField scheduleTopicTextField;
+    TextField scheduleLinkTextField;
+    TextField scheduleCriteriaTextField;
+    GridPane calendarBoundariesPane;
+    GridPane scheduleItemsPane;
+    ///////////////////projects/////////////////////////
+    VBox projectsPane;
+    Label projectsHeader;
+   
+    ///////////
+    GridPane teamsPane;
+    Label teamsHeaderLabel;
+    TableView teamsTable;
+    TableColumn teamsNameColumn;
+    TableColumn teamsColorColumn;
+    TableColumn teamsTextColorColumn;
+    TableColumn teamsLinkColumn;
+    Label teamsAddEditLabel;
+    Label teamsNameLabel;
+    Label teamsColorLabel;
+    Label teamsTextColorLabel;
+    Label teamsLinkLabel;
+    Button teamsAddUpdateButton;
+    Button teamsClearButton;
+    TextField teamsNameTextField;
+    TextField teamsLinkTextField;
+    ColorPicker teamsColorColorPicker;
+    ColorPicker teamsTextColorPicker;
+    
+   
+    //
+    GridPane studentsPane;
+    Label studentsHeaderLabel;
+    TableView studentsTable;
+    TableColumn studentsFirstNameColumn;
+    TableColumn studentsLastNameColumn;
+    TableColumn studentsTeamColumn;
+    TableColumn studentsRoleColumn;
+    Label studentsAddEditLabel;
+    Label studentsFirstNameLabel;
+    TextField studentsFirstNameTextField;
+    Label studentsLastNameLabel;
+    TextField studentsLastNameTextField;
+    Label studentsTeamLabel;
+    ComboBox studentsTeamComboBox;
+    Label studentsRoleLabel;
+    TextField studentsRoleTextField;
+    Button studentsAddUpdateButton;
+    Button studentsClearButton;
+    
+    
+    
     
     
     
@@ -254,11 +332,11 @@ public class CourseSiteGeneratorWorkspace extends AppWorkspaceComponent{
         sitePages = new TableView();
         sitePages.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         siteTemplatePane.add(sitePages, 0, 5);
+        //Set up Table
         useColumn = new TableColumn("Use");
         navbarTitleColumn = new TableColumn("Navbar Title");
         fileNameColumn = new TableColumn("File Name");
         scriptColumn = new TableColumn("Script");
-        
         sitePages.getColumns().add(useColumn);
         sitePages.getColumns().add(navbarTitleColumn);
         sitePages.getColumns().add(fileNameColumn);
@@ -308,7 +386,6 @@ public class CourseSiteGeneratorWorkspace extends AppWorkspaceComponent{
         
         
         recitationDataPane = new GridPane();
-        ;
         recitationLabel = new Label();
         recitationLabel.setText("Recitation");
         recitationDataPane.add(recitationLabel, 0, 0);
@@ -348,17 +425,215 @@ public class CourseSiteGeneratorWorkspace extends AppWorkspaceComponent{
         recitationDataPane.add(firstTAComboBox, 1, 7);
         recitationDataPane.add(secondTAComboBox, 1, 8); 
         
-        addUpdate = new Button();
-        addUpdate.setText("Add/Update");
-        clear = new Button();
-        clear.setText("Clear");
-        recitationDataPane.add(addUpdate, 0, 9);
-        recitationDataPane.add(clear, 1, 9);
+        recitationAddUpdateButton = new Button();
+        recitationAddUpdateButton.setText("Add/Update");
+        recitationClearButton = new Button();
+        recitationClearButton.setText("Clear");
+        recitationDataPane.add(recitationAddUpdateButton, 0, 9);
+        recitationDataPane.add(recitationClearButton, 1, 9);
     
         
       
        recitationTab.setContent(recitationDataPane);
        courseTab.setContent(courseDetailsPane);
+       
+       //////////////////////////////////SCHEDULE/////////////////////////////////////
+       schedulePagePane = new VBox();
+       calendarBoundariesPane = new GridPane();
+       scheduleItemsPane = new GridPane();
+       
+       calendarBoundariesLabel = new Label();
+       calendarBoundariesLabel.setText("Calendar Boundaries");
+       startingMondayLabel = new Label();
+       startingMondayLabel.setText("Starting Monday: ");
+       endingFridayLabel = new Label();
+       endingFridayLabel.setText("Ending Friday: ");
+       mondayDate = new DatePicker();
+       fridayDate = new DatePicker();
+       calendarBoundariesPane.add(calendarBoundariesLabel,0,0);
+       calendarBoundariesPane.add(startingMondayLabel,0,1);
+       calendarBoundariesPane.add(mondayDate,1,1);
+       calendarBoundariesPane.add(endingFridayLabel,2, 1);
+       calendarBoundariesPane.add(fridayDate, 3, 1);
+       
+       //Schedule Items section
+       scheduleItemsLabel = new Label();
+       scheduleItemsLabel.setText("Schedule Items");
+       //SET UP THE TABLE
+       scheduleItemsTable = new TableView();
+       typeColumn = new TableColumn("Type");
+       dateColumn = new TableColumn("Date");
+       titleColumn = new TableColumn("Title");
+       topicColumn = new TableColumn("Topic");
+       scheduleItemsTable.getColumns().add(typeColumn);
+       scheduleItemsTable.getColumns().add(dateColumn);
+       scheduleItemsTable.getColumns().add(titleColumn);
+       scheduleItemsTable.getColumns().add(topicColumn);
+       scheduleItemsPane.add(scheduleItemsLabel, 0, 0);
+       scheduleItemsPane.add(scheduleItemsTable, 0, 1);
+       
+       scheduleAddEditLabel = new Label();
+       scheduleAddEditLabel.setText("ADD/EDIT");
+       typeLabel = new Label();
+       typeLabel.setText("Type: ");
+       dateLabel = new Label();
+       typeComboBox = new ComboBox();
+       scheduleItemsPane.add(scheduleAddEditLabel, 0, 2);
+       dateLabel.setText("Date: ");
+       timeLabel = new Label();
+       timeLabel.setText("Time: ");
+       titleLabel = new Label();
+       titleLabel.setText("Title: ");
+       topicLabel = new Label();
+       topicLabel.setText("Topic: ");
+       linkLabel = new Label();
+       linkLabel.setText("Link: ");
+       criteriaLabel = new Label();
+       criteriaLabel.setText("Criteria: ");
+       scheduleDate = new DatePicker();
+       scheduleTimeTextField = new TextField();
+       scheduleTitleTextField = new TextField();
+       scheduleTopicTextField = new TextField();
+       scheduleLinkTextField = new TextField();
+       scheduleCriteriaTextField = new TextField();
+       
+       
+       
+       
+       scheduleItemsPane.add(typeLabel,0,3);
+       scheduleItemsPane.add(dateLabel,0,4);
+       scheduleItemsPane.add(timeLabel,0,5);
+       scheduleItemsPane.add(titleLabel,0,6);
+       scheduleItemsPane.add(topicLabel,0,7);
+       scheduleItemsPane.add(linkLabel,0,8);
+       scheduleItemsPane.add(criteriaLabel,0,9);
+       scheduleItemsPane.add(typeComboBox, 1,3);
+       scheduleItemsPane.add(scheduleDate, 1,4);
+       scheduleItemsPane.add(scheduleTimeTextField, 1,5);
+       scheduleItemsPane.add(scheduleTitleTextField, 1,6);
+       scheduleItemsPane.add(scheduleTopicTextField, 1,7);
+       scheduleItemsPane.add(scheduleLinkTextField, 1,8);
+       scheduleItemsPane.add(scheduleCriteriaTextField, 1,9);
+       
+       schedulePagePane.getChildren().add(calendarBoundariesPane);
+       schedulePagePane.getChildren().add(scheduleItemsPane);
+       scheduleTab.setContent(schedulePagePane);
+       
+       //////////////////////////////PROJECTs//////////////////
+       
+       //Initialize all the panes
+       projectsPane = new VBox();
+       teamsPane = new GridPane();
+       studentsPane = new GridPane();
+       
+       projectsHeader = new Label();
+       projectsHeader.setText("Projects");
+       projectsPane.getChildren().add(projectsHeader);
+       
+       teamsHeaderLabel = new Label();
+       teamsHeaderLabel.setText("Teams");
+       teamsTable = new TableView();
+       teamsNameColumn = new TableColumn();
+       teamsNameColumn.setText("Name");
+       teamsColorColumn = new TableColumn();
+       teamsColorColumn.setText("Color (hex#)");
+       teamsTextColorColumn = new TableColumn();
+       teamsTextColorColumn.setText("Text Color (hex#)");
+       teamsLinkColumn = new TableColumn();
+       teamsLinkColumn.setText("Link");
+       teamsTable.getColumns().addAll(teamsNameColumn,
+               teamsColorColumn, 
+               teamsTextColorColumn,
+               teamsLinkColumn);
+       teamsPane.add(teamsHeaderLabel,0,0);
+       teamsPane.add(teamsTable,0,1);
+       teamsAddEditLabel = new Label();
+       teamsAddEditLabel.setText("Add/Edit");
+       teamsNameLabel = new Label();
+       teamsNameLabel.setText("Name: ");
+       teamsColorLabel = new Label();
+       teamsColorLabel.setText("Color: ");
+       teamsColorColorPicker = new ColorPicker();
+       teamsTextColorPicker = new ColorPicker();
+       teamsTextColorLabel = new Label();
+       teamsTextColorLabel.setText("Text Color: ");
+       teamsLinkLabel = new Label();
+       teamsLinkLabel.setText("Link");
+       teamsLinkTextField = new TextField();
+       teamsAddUpdateButton = new Button();
+       teamsAddUpdateButton.setText("Add/Update");
+       teamsClearButton = new Button();
+       teamsClearButton.setText("Clear");
+       teamsPane.add(teamsAddEditLabel, 0,2);
+       teamsPane.add(teamsNameLabel,0,3);
+       teamsNameTextField = new TextField();
+       teamsPane.add(teamsNameTextField,1,3);
+       teamsPane.add(teamsColorLabel,0,4);
+       teamsPane.add(teamsColorColorPicker,1,4);
+       teamsPane.add(teamsTextColorLabel,2,4);
+       teamsPane.add(teamsTextColorPicker,3,4);
+       teamsPane.add(teamsLinkLabel,0,5);
+       teamsPane.add(teamsLinkTextField,1,5);
+       teamsPane.add(teamsAddUpdateButton, 0,6);
+       teamsPane.add(teamsClearButton,1,6);
+       //////Students pane
+       
+       studentsHeaderLabel = new Label();
+       studentsHeaderLabel.setText("Students");
+       studentsTable = new TableView();
+       studentsFirstNameColumn = new TableColumn();
+       studentsFirstNameColumn.setText("First Name");
+       studentsLastNameColumn = new TableColumn();
+       studentsLastNameColumn.setText("Last Name");
+       studentsTeamColumn = new TableColumn();
+       studentsTeamColumn.setText("Team");
+       studentsRoleColumn = new TableColumn();
+       studentsRoleColumn.setText("Role");
+       studentsTable.getColumns().addAll(
+            studentsFirstNameColumn,
+            studentsLastNameColumn,
+            studentsTeamColumn,
+            studentsRoleColumn);
+       studentsPane.add(studentsHeaderLabel,0,0);
+       studentsPane.add(studentsTable,0,1);
+       studentsAddEditLabel = new Label();
+       studentsAddEditLabel.setText("Add/Edit");
+       studentsFirstNameLabel = new Label();
+       studentsFirstNameLabel.setText("First Name:");
+       studentsLastNameLabel = new Label();
+       studentsLastNameLabel.setText("Last Name: ");
+       studentsTeamLabel= new Label();
+       studentsTeamLabel.setText("Team: ");
+       studentsRoleLabel = new Label();
+       studentsRoleLabel.setText("Role: ");
+       studentsFirstNameTextField = new TextField();
+       studentsLastNameTextField = new TextField();
+       studentsTeamComboBox = new ComboBox();
+       studentsRoleTextField = new TextField();
+       studentsAddUpdateButton = new Button();
+       studentsAddUpdateButton.setText("Add/Update");
+       studentsClearButton = new Button();
+       studentsClearButton.setText("Clear");
+       studentsPane.add(studentsAddEditLabel,0,2);
+       studentsPane.add(studentsFirstNameLabel,0,3);
+       studentsPane.add(studentsFirstNameTextField,1,3);
+       studentsPane.add(studentsLastNameLabel,0,4);
+       studentsPane.add(studentsLastNameTextField,1,4);
+       studentsPane.add(studentsTeamLabel,0,5);
+       studentsPane.add(studentsTeamComboBox,1,5);
+       studentsPane.add(studentsRoleLabel,0,6);
+       studentsPane.add(studentsRoleTextField,1,6);
+       studentsPane.add(studentsAddUpdateButton,0,7);
+       studentsPane.add(studentsClearButton,1,7);
+       
+       projectsPane.getChildren().add(teamsPane);
+       projectsPane.getChildren().add(studentsPane);
+       projectTab.setContent(projectsPane);
+       
+       
+       
+       
+       
        
     }
 
